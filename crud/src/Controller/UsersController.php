b60b83eb0b9922cef0f6fb1ab7946c539d469ead
stas_lampdev <lampdev.dev7@gmail.com>
2020-12-25 23:50:24 +0200

    Show action created.

--- crud/src/Controller/UsersController.php
+++ crud/src/Controller/UsersController.php
@@ -2,10 +2,42 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UsersController extends AbstractController
 {
+    /** @var UserRepository */
+    private $userRepository;
+
+    public function __construct(UserRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
+    /**
+     * @Route("/users/show/{id}", name="users.show", methods={"GET"})
+     * @param int $id
+     * @return JsonResponse
+     */
+    public function showAction(int $id): JsonResponse
+    {
+        /** @var User|null $user */
+        $user = $this->userRepository->find($id);
+
+        if (!$user) {
+            return new JsonResponse(null, Response::HTTP_NOT_FOUND);
+        }
+
+        return new JsonResponse([
+            'id' => $user->getId(),
+            'name' => $user->getName(),
+            'email' => $user->getEmail(),
+            'role_id' => $user->getRole()->getId()
+        ]);
+    }
 }
