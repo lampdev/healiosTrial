96aad4dcc20f45cae844756e31ff21a505e693f1
stas_lampdev <lampdev.dev7@gmail.com>
2020-12-30 19:03:15 +0200

    Store action done

--- crud/src/Controller/TokensController.php
+++ crud/src/Controller/TokensController.php
@@ -2,8 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\AccessToken;
+use App\Entity\User;
+use App\Repository\AccessTokenRepository;
+use App\Repository\UserRepository;
+use HealiosTrial\Services\JsonRequestDataKeeper;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -11,6 +17,18 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class TokensController
 {
+    /** @var UserRepository */
+    private $userRepository;
+
+    /** @var AccessTokenRepository */
+    private $accessTokenRepository;
+
+    public function __construct(UserRepository $userRepository, AccessTokenRepository $accessTokenRepository)
+    {
+        $this->userRepository = $userRepository;
+        $this->accessTokenRepository = $accessTokenRepository;
+    }
+
     /**
      * @Route("/store", name="store", methods={"POST"})
      * @param Request $request
@@ -18,6 +36,22 @@ class TokensController
      */
     public function storeAction(Request $request): JsonResponse
     {
-        return new JsonResponse();
+        $request = JsonRequestDataKeeper::keepJson($request);
+        $userId = (int)$request->get('user_id', 0);
+
+        /** @var User|null $user */
+        $user = $this->userRepository->find($userId);
+
+        if (!$user) {
+            return new JsonResponse(['errors' => 'User not found', Response::HTTP_NOT_FOUND]);
+        }
+
+        $token = (string)$request->get('token', '');
+        $tokenModel = new AccessToken();
+        $tokenModel->setToken($token);
+        $tokenModel->setUser($user);
+        $this->accessTokenRepository->plush($tokenModel);
+
+        return new JsonResponse(['message' => 'Token stored']);
     }
 }
