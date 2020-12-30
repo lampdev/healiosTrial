9c637ed0756f7b3d8b751fdfffa380b0deecb426
stas_lampdev <lampdev.dev7@gmail.com>
2020-12-30 13:32:43 +0200

    Phpunit installed in crud

---
+++ crud/tests/Unit/RolesManagerTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class RolesManagerTest extends TestCase
+{
+    public function testTmp()
+    {
+        $this->assertTrue(true);
+    }
+}
